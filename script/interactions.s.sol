698217fde769f8b8b3cd2936f8ee4419b84e4909
GoudhamT <goudhamvel.88@gmail.com>
2025-11-29 19:54:38 -0800

    DevOps implementation

--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -2,7 +2,23 @@
 
 pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
 
 contract BasicNFTInteractions is Script {
-    function run() public {}
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() public {
+        address recentlyDeployedAddress = DevOpsTools
+            .get_most_recent_deployment("BasicNFT", block.chainid);
+
+        mintNFTOnAddress(recentlyDeployedAddress);
+    }
+
+    function mintNFTOnAddress(address _recentlyDeployed) public {
+        vm.startBroadcast();
+        BasicNFT(_recentlyDeployed).mintNFT(PUG_URI);
+        vm.stopBroadcast();
+    }
 }
