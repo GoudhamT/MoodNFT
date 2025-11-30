1f8ea858eb70f31820e867a51faf267361efa330
GoudhamT <goudhamvel.88@gmail.com>
2025-11-29 20:24:38 -0800

    contract deployed

--- makefile
+++ makefile
@@ -12,3 +12,6 @@ gas :; forge snapshot
 
 deploy-sepolia:
 	forge script script/DeployBasicNFT.s.sol:DeployBasicNFT --rpc-url $(SEPOLIA_RPC_URL) --private-key $(SEPOLIA_PRIVATE_KEY) --broadcast --verify --etherscan-api-key $(ETHERSCAN_API_KEY) -vvvv
+
+mint-NFT:
+	forge script script/interactions.s.sol:BasicNFTInteractions --rpc-url $(SEPOLIA_RPC_URL) --private-key $(SEPOLIA_PRIVATE_KEY) --broadcast
--- script/interactions.s.sol
+++ script/interactions.s.sol
@@ -10,9 +10,9 @@ contract BasicNFTInteractions is Script {
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run() public {
-        address recentlyDeployedAddress = DevOpsTools
-            .get_most_recent_deployment("BasicNFT", block.chainid);
-
+        // address recentlyDeployedAddress = DevOpsTools
+        //     .get_most_recent_deployment("BasicNFT", block.chainid);
+        address recentlyDeployedAddress = vm.envAddress("BASIC_NFT_ADDRESS");
         mintNFTOnAddress(recentlyDeployedAddress);
     }
 
